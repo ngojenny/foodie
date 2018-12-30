a3c5a57778d8d22c8a57bb19e49c0ecff83ebe15
Jenny <jennyngo.dev@gmail.com>
2018-12-29 23:50:02 -0500

    set up firebase authentication

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import firebase from 'firebase';
+import './App.scss';
 
 import Title from './components/Title/Title';
 import Button from './components/Button/Button';
 
-const config = {
+var config = {
   apiKey: "AIzaSyDFAU2wNQuaPOrN10MIaI8RiHKC3amjwzg",
   authDomain: "foodie-f2a16.firebaseapp.com",
   databaseURL: "https://foodie-f2a16.firebaseio.com",
@@ -12,12 +13,38 @@ const config = {
   storageBucket: "foodie-f2a16.appspot.com",
   messagingSenderId: "283200990093"
 };
-
 firebase.initializeApp(config);
 
+const provider = new firebase.auth.GoogleAuthProvider();
+const auth = firebase.auth();
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      user: null
+    }
+  }
+
+  componentDidMount() {
+    auth.onAuthStateChanged((user) => {
+      if(user) {
+        this.setState({
+          user
+        })
+      }
+    })
+  }
+
   login = () =>{
-    console.log('logging in')
+    auth.signInWithPopup(provider)
+    .then((result) => {
+      const user = result.user;
+      this.setState({
+        user
+      })
+      console.log(result);
+    })
   }
   render() {
     return (
