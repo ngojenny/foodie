f2dbe053501cb540bd35f87072b8c51b9d62ca2e
Jenny <jennyngo.dev@gmail.com>
2018-12-29 21:22:53 -0500

    setup project, start file structure add sass

---
+++ src/components/Button/Button.js
@@ -0,0 +1,18 @@
+import React, { Fragment } from 'react';
+import Link from 'react-router-dom';
+
+const Button = (props) => {
+  return (
+    <Fragment>
+      {props.category === 'button' &&
+        <button onClick={props.eventHandler} className={`btn btn--${props.type}`}>{props.text}</button>
+      }
+      {props.category === 'link' &&
+        <Link className={`btn btn--${props.type}`}>{props.text}</Link>
+      }
+    </Fragment>
+  )
+}
+
+export default Button
+
